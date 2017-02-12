8ec0cf9bd1a967315a705f02c643940f224198bc
Курбан Курбанов <qurban.qranov93@gmail.com>
2017-02-13 02:50:15 +0400

    MAIN PAGE - TED block finished

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     $('.js-tilt').tilt({
         maxTilt: 9,
-        glare: true,
-        maxGlare: .5,
         scale: 1.05
     });
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,33 @@
 /**
  * Created by BumbuKhan on 09.02.2017.
  */
+'use strict';
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
 // watch files for change and reload
 gulp.task('serve', function () {
-   browserSync({
-       server: {
-           baseDir: 'app'
-       }
-   });
+    browserSync({
+        server: {
+            baseDir: 'app'
+        }
+    });
 
-   gulp.watch(['*.html', 'css/**/*.css', 'js/**/*.js'], {cwd: 'app'}, reload);
+    gulp.watch(['*.html', 'css/**/*.css', 'js/**/*.js'], {cwd: 'app'}, reload);
+});
+
+// compile from SCSS to CSS
+gulp.task('sass', function () {
+    return gulp.src('./app/css/sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./app/css/sass/**/*.scss', ['sass']);
 });
