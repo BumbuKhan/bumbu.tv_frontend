68b0784346789a55dd2f56531fc7a9b79cb7c6c3
Курбан Курбанов <qurban.qranov93@gmail.com>
2017-02-13 23:30:21 +0400

    MAIN PAGE - animation form series block is removed

--- app/js/app.js
+++ app/js/app.js
@@ -3,14 +3,4 @@ $(document).ready(function () {
         maxTilt: 9,
         scale: 1.05
     });
-
-    var seriesItems = $('.js-series-item');
-
-    seriesItems.hover(function(){
-        $(this).removeClass('series-item-mouse-out');
-        $(this).addClass('series-item-mouse-in');
-    }, function(){
-        $(this).removeClass('series-item-mouse-in');
-        $(this).addClass('series-item-mouse-out');
-    });
 });
