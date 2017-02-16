3b3ad2159f32b10c6d83cf2e99e29ca9d3886e7f
Курбан Курбанов <qurban.qranov93@gmail.com>
2017-02-17 01:53:34 +0400

    VIDEO PLAYER - playing around the params

--- app/src/js/video-player.js
+++ app/src/js/video-player.js
@@ -1,8 +1,45 @@
 /**
  * Created by BumbuKhan on 16.02.2017.
  */
+
+// The actual default component structure of the Video.js player looks something like this:
+
+// Player
+//      PosterImage
+//      TextTrackDisplay
+//      LoadingSpinner
+//      BigPlayButton
+//      ControlBar
+//          PlayToggle
+//          FullscreenToggle
+//          CurrentTimeDisplay
+//          TimeDivider
+//          DurationDisplay
+//          RemainingTimeDisplay
+//          ProgressControl
+//              SeekBar
+//                  LoadProgressBar
+//                  PlayProgressBar
+//                  SeekHandle
+//          VolumeControl
+//              VolumeBar
+//                  VolumeLevel
+//                  VolumeHandle
+//          MuteToggle
+
+// read more http://docs.videojs.com/docs/guides/components.html
+
 $(document).ready(function () {
-    videojs("bumbu-player", {}, function(){
+    var bumbuPlayer = videojs('bumbu-player', {
+        controlBar: {
+            muteToggle: false,
+            fullscreenToggle: false,
+            remainingTimeDisplay: false,
+            progressControl: false
+        },
+        "controls": true,
+        "preload": "auto"
+    }, function () {
         console.log('Player is initialized and ready.');
     });
 });
